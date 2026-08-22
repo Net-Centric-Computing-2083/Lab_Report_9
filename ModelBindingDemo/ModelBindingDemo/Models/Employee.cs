5d5479ab7f7ce891c5a890c023a58890cdcbe567
kushalpahadi <kushalpahadi132@gmail.com>
2026-08-22 17:08:22 +0545

    Check ModelBinding Demo

---
+++ ModelBindingDemo/ModelBindingDemo/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ModelBindingDemo.Models { 
+    public class Employee { 
+        [Required] 
+        public int Id { get; set; } 
+        [Required] 
+        public string Name { get; set; } = string.Empty; 
+        [Required][EmailAddress] 
+        public string Email { get; set; } = string.Empty; 
+        [Range(18, 100)] 
+        public int Age { get; set; } 
+        [Required] 
+        public string ContactNo { get; set; } = string.Empty;
+        [Range(0, 100000000)] 
+        public decimal Salary { get; set; } 
+        [Required][DataType(DataType.Date)] 
+        public DateTime JoinedDate { get; set; } 
+    } 
+}
