91991981c0b9a85a08d730f3b46480a5048d3421
preshusingh22@gmail.com <preshusingh22@gmail.com>
2026-09-01 09:39:38 +0545

    lab-9

---
+++ Lab_Report_9/Controllers/EmployeeController1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab_Report_9.Models;
+
+namespace Lab_Report_9.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Result", employee);
+            }
+
+            return View(employee);
+        }
+    }
+}
