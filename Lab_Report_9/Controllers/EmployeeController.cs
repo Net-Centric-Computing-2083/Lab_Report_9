127e1eb5456f16f61855d7409d236e29163b1221
Rojesh Niroula <wonderingmind2005@gmail.com>
2026-09-05 10:06:51 +0545

    This is my lab 9

---
+++ Lab_Report_9/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using Lab_Report_9.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_Report_9.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: /Employee/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Employee/Create
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Redisplay the form with validation messages if data is invalid.
+                return View(employee);
+            }
+
+            // Model Binding has already mapped the form fields to the Employee object.
+            return View("Result", employee);
+        }
+    }
+}
---
+++ Lab_Report_9/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab_Report_9.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Range(18, 65)]
+        public int Age { get; set; }
+
+        public string ContactNo { get; set; }
+
+        public decimal Salary { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime JoinedDate { get; set; }
+    }
+}
