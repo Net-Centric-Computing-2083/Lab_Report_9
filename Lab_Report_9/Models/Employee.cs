5f4cefd5f09fbf3ec3aacaf882bd5c972c97536b
samriddha77 <poudelsamriddha77@gmail.com>
2026-09-06 21:17:14 +0545

    lab9

---
+++ Lab_Report_9/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LabReport9.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Range(18, 65)]
+        public int Age { get; set; }
+
+        [Required]
+        public string ContactNo { get; set; }
+
+        [Required]
+        [Range(0, 10000000)]
+        public decimal Salary { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime JoinedDate { get; set; }
+    }
+}
