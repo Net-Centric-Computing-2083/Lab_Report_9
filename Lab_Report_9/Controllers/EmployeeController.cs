aee7e7f1d393bd8b02ffe6066e2d0cfd4ee00a27
sumit.neupane001 <sumit.neupane2062@gmail.com>
2026-09-07 08:36:37 +0545

    This is lab 9

---
+++ Lab_Report_9/Controllers/EmployeeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMvcApp.Models;
+namespace MyMvcApp.Controllers {
+    public class EmployeeController : Controller { 
+        // GET: Employee/Create
+        [HttpGet] public IActionResult Create() 
+        { return View(); }
+        // POST: Employee/Create
+        [HttpPost] public IActionResult Create(Employee employee) 
+        { if (ModelState.IsValid) {
+                return View("Result", employee); }
+            return View(employee);
+        } } }
---
+++ Lab_Report_9/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace MyMvcApp.Models { 
+    public class Employee { 
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Range(18, 60)]
+        public int Age { get; set; }
+        [Required] 
+        public string ContactNo { get; set; }
+        [Required]
+        [Range(0, 10000000)] 
+        public decimal Salary { get; set; } 
+        [Required]
+        [DataType(DataType.Date)] 
+        public DateTime JoinedDate { get; set;
+        } } }
