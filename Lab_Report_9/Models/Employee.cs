cacec242397007a8a31a9611103f92185b91a218
Acer <Acer@SUSHANT>
2026-09-07 07:41:53 +0545

    LAB 9 COMMITTED

---
+++ Lab_Report_9/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab_Report_9.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string ContactNo { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime JoinedDate { get; set; }
+    }
+}
