ce9af009ba91a6c6e346b1e0d3222e154c786a3a
gayatri <gayatrijoshi2063@gmail.com>
2026-09-09 08:34:02 +0545

    lab 9

---
+++ Lab_Report_9/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBindingDemo.Models;
+
+namespace ModelBindingDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Result", employee);
+            }
+
+            return View(employee);
+        }
+    }
+}
---
+++ Lab_Report_9/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingDemo.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email address")]
+        public string? Email { get; set; }
+
+        [Range(18, 60, ErrorMessage = "Age must be between 18 and 60")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Contact number is required")]
+        public string ?ContactNo { get; set; }
+
+        [Range(0, 10000000, ErrorMessage = "Enter a valid salary")]
+        public decimal Salary { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime JoinedDate { get; set; }
+    }
+}
