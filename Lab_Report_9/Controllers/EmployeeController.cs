53cfadd862323d1a37b8b557737efef1bea37256
rusha13 <rushapokharel13@gmail.com>
2026-09-09 10:03:58 +0545

    Final program.

---
+++ Lab_Report_9/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Lab9.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab9.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            return View("Result", employee);
+        }
+    }
+}
