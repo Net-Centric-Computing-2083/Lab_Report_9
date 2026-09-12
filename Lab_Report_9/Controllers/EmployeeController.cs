55fffc84d9b1bad27657e80220d1bbe95b440e94
alkeshchaudhary885@gmail.com <alkeshchaudhary885@gmail.com>
2026-09-12 19:59:52 +0545

    Add Lab 9 project files

---
+++ Lab_Report_9/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using Lab_Report_9.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_Report_9.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // Temporary storage for submitted employee
+        private static Employee submittedEmployee;
+
+        // GET: /Employee/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Employee/Create
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            submittedEmployee = employee;
+
+            return RedirectToAction("Details");
+        }
+
+        // GET: /Employee/Details
+        [HttpGet]
+        public IActionResult Details()
+        {
+            if (submittedEmployee == null)
+            {
+                return Content("No employee information has been submitted yet.");
+            }
+
+            return View(submittedEmployee);
+        }
+    }
+}
---
+++ Lab_Report_9/Views/Employee/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab_Report_9.Views.Employee
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
