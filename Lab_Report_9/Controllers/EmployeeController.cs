299e26042084e8b5586df92edc6a680d939a3e25
Shreebisha Shrestha <shreebishacsit23@oic.edu.np>
2026-09-16 22:27:21 +0545

    Initial Commit

---
+++ Lab_Report_9/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab_Report_9.Models;
+
+namespace Lab_Report_9.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Result", employee);
+            }
+
+            return View(employee);
+        }
+    }
+}
---
+++ Lab_Report_9/Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab_Report_9.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [Range(18, 100)]
+        public int Age { get; set; }
+
+        [Required]
+        [Display(Name = "Contact No")]
+        public string ContactNo { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Salary { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Joined Date")]
+        public DateTime JoinedDate { get; set; }
+    }
+}
